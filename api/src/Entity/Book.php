2526592dc6b8e2e60265cb1cec30cdc364757bdd
francois-hesias <Francois.TEIL@hesias.fr>
2021-12-17 05:30:38 -0800

    book fin de tp

--- api/src/Entity/Book.php
+++ api/src/Entity/Book.php
@@ -4,20 +4,39 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 #[ApiResource(
     collectionOperations: [
-        'post', 'get'
+        'post', 'get',
+        'get_oui' => [
+            'method' => 'GET',
+            'path' => '/books/oui',
+            'status' => 200,
+        ],
+    "create" => [
+        "method" => "POST",
+        "input" => creatbook::class,
+        "output" => dtobook::class
+    ],
     ],
     itemOperations: [
         'get', 'delete', 'put'
     ],
+    normalizationContext : ['groups' => ['books:read']]
+
 )]
 #[ApiFilter(SearchFilter::class, properties: ['titre' => 'ipartial'])]
+#[ApiFilter(DateFilter::class, properties: ['createdAt'])]
+#[ApiFilter(BooleanFilter::class, properties: ['Dispo'])]
+#[ApiFilter(OrderFilter::class, properties: ['titre'])]
 class Book
 {
     #[ORM\Id]
@@ -26,7 +45,16 @@ class Book
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['books:read'])]
     private $titre;
+
+
+    #[ORM\Column(type: 'boolean')]
+    #[Groups(['books:read'])]
+    private $dispo;
+
+    #[ORM\Column(type: 'datetime')]
+    private $createdAt;
 
     public function getId(): ?int
     {
@@ -44,4 +72,34 @@ class Book
 
         return $this;
     }
+
+
+    /**
+     * @return mixed
+     */
+    public function getDispo()
+    {
+        return $this->dispo;
+    }
+
+    /**
+     * @param mixed $dispo
+     */
+    public function setDispo($dispo): void
+    {
+        $this->dispo = $dispo;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
 }
